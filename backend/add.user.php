630bedb9c113ca7870803c8c167190a20c32445a
itsrusty <rusty3031@gmail.com>
2022-11-03 21:58:31 -0700

    add backend feauture

---
+++ backend/add.user.php
@@ -0,0 +1,17 @@
+<?php
+    include("./bd.php");
+    if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        // add to bd 
+        $sql = "INSERT INTO users (name_user, email_user, password_user) VALUES ('$name', '$email', '$password')";
+        $result = mysqli_query($conex, $sql);
+        if($result){
+            header("Location: ../views/index.php");
+        }else{
+            header("Location: ../views/register.php");
+        }
+    }
+?>
