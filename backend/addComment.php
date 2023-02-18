b5a62b4e8d95fe0a8407d1585e3c8de23927309f
itsrusty <rusty3031@gmail.com>
2023-02-18 11:57:46 -0700

    add logic to save comment in table mysql

--- backend/addComment.php
+++ backend/addComment.php
@@ -1,9 +1,12 @@
 <?php
     include("./bd.php");
 
-    if(isset($_POST['submit']) && ){
-        $comment = $_POST['comment'];
-
-        // $query_comment = "SELECT * FROM comments WHERE comment
+    if(isset($_POST['submit'])){
+        $comment = $_POST['comentario'];
+        $query_comment = "INSERT INTO comments (comment) VALUES ('$comment')";
+        $result_comment = mysqli_query($conex,$query_comment);
+        if($result_comment){
+            header("../views/index.php");
+        }
     }
 ?>
