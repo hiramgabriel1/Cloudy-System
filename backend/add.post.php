e70c4c963c4bdfe6c072f40ed0e902bcca135bc1
itsrusty <rusty3031@gmail.com>
2023-02-17 19:53:06 -0700

    resolve bugs

--- backend/add.post.php
+++ backend/add.post.php
@@ -1,17 +1,27 @@
 <?php
     include("./bd.php");
-    if(isset($_GET["data-post-text"])){
+    if(isset($_POST["submit"])){
         // only text simple
-        $data_post_text_simple = $_GET['data-post-text'];
-        // only files
-        $data_post_files = $_GET['data-post-files'];
+        $data_post_text_simple = $_POST['data-post-text'];
         // title post
-        $title_post = $_GET['title-post'];
+        $title_post = $_POST['title-post'];
 
-        $QUERY = "INSERT INTO posts (title_post, description_post) VALUES ('$title_post','$data_post_text_simple')";
+        $nameImage = $_FILES["imagen"]["name"];
+        $archivo = $_FILES["imagen"]["tmp_name"];
+        $ruta="../images";
+
+        $ruta=$ruta."/".$nameImage;
+        move_uploaded_file($archivo, $ruta);
+
+        $QUERY = "INSERT INTO posts (title_post, description_post, ruta_file) VALUES ('$title_post','$data_post_text_simple', '$ruta')";
         $result_query = mysqli_query($conex, $QUERY);
         if($result_query){
             echo "funciona";
+            // header("../views/index.php");
         }
+
+        // error_reporting(E_ALL);
+        // ini_set("display_errors", "1");
+        ini_set('error_reporting', E_ERROR | E_PARSE);
     }
 ?>
