9978ccd33731f50f180ae3dd15a2ed6e29236f18
itsrusty <rusty3031@gmail.com>
2023-02-18 11:18:56 -0700

    render

--- views/index.php
+++ views/index.php
@@ -192,8 +192,8 @@
               </style>
 
               <!-- tipo de post 1 -->
-                  <!-- render data -->
-               <?php   
+              <!-- render data -->
+              <?php   
                   include("../backend/bd.php");    
                   $query = "SELECT * FROM posts";
                   $result = mysqli_query($conex, $query);
@@ -206,13 +206,7 @@
                     alt="Profile Picture"
                   />
                   <div class="header-text">
-                  <?php  
-                  include("../backend/bd.php");
-                  $query_users = "SELECT * FROM users";
-                  $result_users = mysqli_query($conex, $query_users);
-                  while($fila_users = mysqli_fetch_array($result_users)){
-                ?>
-                    <h3><?php echo $fila_users['name_user']; } ?></h3>
+                    <h3>Usuario</h3>
                     <p><?php echo $fila['fecha']?></p>
                   </div>
                 </div>
@@ -221,7 +215,10 @@
                     <?php echo $fila['description_post']; ?>
                   </p>
                   <!-- foto del post -->
-                    <img src="<?php echo $fila['ruta_file'];?>" alt="Post Image" />
+                  <img
+                    src="<?php echo $fila['ruta_file'];?>"
+                    alt="Post Image"
+                  />
                 </div>
                 <div class="card-footer">
                   <button>Me Gusta</button>
@@ -314,160 +311,6 @@
                   }
                 }
               </style>
-              <!-- tipo de post 2-->
-              <div class="album box">
-                <div class="status-main">
-                  <div class="album-detail">
-                    <div class="album-title">
-                      Imagenes por IA <span>Informática</span>
-                    </div>
-                    <div class="album-date">6 hours ago</div>
-                  </div>
-                  <button class="intro-menu"></button>
-                </div>
-                <div class="album-content">
-                  Acabo de crear estas imagenes con inteligencia artificial
-                  usando python
-                  <div class="album-photos">
-                    <img
-                      src="https://images.unsplash.com/photo-1508179719682-dbc62681c355?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2378&q=80"
-                      alt=""
-                      class="album-photo"
-                    />
-                    <div class="album-right">
-                      <img
-                        src="https://images.unsplash.com/photo-1502872364588-894d7d6ddfab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"
-                        alt=""
-                        class="album-photo"
-                      />
-                      <img
-                        src="https://images.unsplash.com/photo-1566737236500-c8ac43014a67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                        alt=""
-                        class="album-photo"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div class="album-actions">
-                  <a href="#" class="album-action">
-                    <svg
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"
-                      />
-                    </svg>
-                    87
-                  </a>
-                  <a href="#" class="album-action">
-                    <svg
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="css-i6dzq1"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        d="M21 15a2 2 0 01-2 2H7l-4 4V5a2 2 0 012-2h14a2 2 0 012 2z"
-                      />
-                    </svg>
-                    20
-                  </a>
-                  <a href="#" class="album-action">
-                    <svg
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="css-i6dzq1"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M17 1l4 4-4 4" />
-                      <path d="M3 11V9a4 4 0 014-4h14M7 23l-4-4 4-4" />
-                      <path d="M21 13v2a4 4 0 01-4 4H3" />
-                    </svg>
-                    13
-                  </a>
-                </div>
-              </div>
-
-              <!-- tipo de post 3 -->
-              <div class="album box">
-                <div class="status-main">
-                  <div class="almai-detail">
-                    <div class="album-title">create new <span>album</span></div>
-                    <div class="album-date">6 hours ago</div>
-                  </div>
-                  <button class="intro-menu"></button>
-                </div>
-                <div class="album-content">
-                  When the bass drops, so do my problems.
-                  <div class="album-photos">
-                    <img
-                      src="https://images.unsplash.com/photo-1508179719682-dbc62681c355?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2378&q=80"
-                      alt=""
-                      class="album-photo"
-                    />
-                  </div>
-                </div>
-                <div class="album-actions">
-                  <a href="#" class="album-action">
-                    <svg
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"
-                      />
-                    </svg>
-                    87
-                  </a>
-                  <a href="#" class="album-action">
-                    <svg
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="css-i6dzq1"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        d="M21 15a2 2 0 01-2 2H7l-4 4V5a2 2 0 012-2h14a2 2 0 012 2z"
-                      />
-                    </svg>
-                    20
-                  </a>
-                  <a href="#" class="album-action">
-                    <svg
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="css-i6dzq1"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M17 1l4 4-4 4" />
-                      <path d="M3 11V9a4 4 0 014-4h14M7 23l-4-4 4-4" />
-                      <path d="M21 13v2a4 4 0 01-4 4H3" />
-                    </svg>
-                    13
-                  </a>
-                </div>
-              </div>
             </div>
           </div>
         </div>
