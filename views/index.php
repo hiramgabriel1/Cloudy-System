1a11c6b045c103008ed04906bc6fcb8e4771c5d1
Jerson Tapias <104840746+programadorisgod@users.noreply.github.com>
2023-02-24 10:02:57 -0500

    Add files via upload

--- views/index.php
+++ views/index.php
@@ -4,143 +4,381 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- bootstrap -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-      crossorigin="anonymous"
-    />
-    <!-- materialize.css -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
-    />
-    <!-- bootstrap icon -->
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
-    />
-    <!-- css -->
+    <!-- styles -->
     <link rel="stylesheet" href="../public/styles/index.css" />
-    <!-- <link rel="stylesheet" href="../public/styles/sidebar.mobile.css"> -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+    />
     <title>CloudySystem</title>
   </head>
   <body>
-    <main>
-    <!-- side bar -->
-    <aside
-      class="sidebar position-fixed top-0 left-0 overflow-auto h-100 float-left"
-      id="show-side-navigation1"
-    >
-      <i
-        class="uil-bars close-aside d-md-none d-lg-none"
-        data-close="show-side-navigation1"
-      ></i>
-      <div
-        class="sidebar-header d-flex justify-content-center align-items-center px-3 py-4"
-      >
-        <h1>Cloudy</h1>
-        <div class="ms-2">
-          <h5 class="fs-6 mb-0">
-            <a class="text-decoration-none" href="#"></a>
-          </h5>
+    <div class="container" x-data="{ rightSide: false, leftSide: false }">
+      <div class="left-side visible"  :class="{'active' : leftSide}" id="nav">
+        <div class="left-side-button" @click="leftSide = !leftSide" id="abrir">
+          <svg
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <line x1="3" y1="12" x2="21" y2="12"></line>
+            <line x1="3" y1="6" x2="21" y2="6"></line>
+            <line x1="3" y1="18" x2="21" y2="18"></line>
+          </svg>
+        </div>
+        <div class="logo">CloudySystem</div>
+        <style>
+          @media screen and(max-width:930px) {
+            .logo {
+              visibility: hidden;
+            }
+          }
+        </style>
+        <div class="side-wrapper">
+          <div class="side-title">Herramientas</div>
+          <div class="side-menu">
+            <a href="#"> Inicio </a>
+            <a href="../tools/creator_pdf.html">
+              <span class="material-symbols-outlined"> home </span>
+              Creador PDF
+            </a>
+            <a href="../tools/todo.html">
+              <span class="material-symbols-outlined"> heart_plus </span>
+              Pendientes
+            </a>
+            <a
+              href="../tools/plus.html"
+              class="plus-section"
+              style="color: gold"
+            >
+              <span class="material-symbols-outlined"> star </span>
+              Plus
+            </a>
+          </div>
+        </div>
+        <div class="side-wrapper">
+          <div class="side-title">Más</div>
+          <div class="side-menu">
+            <a href="../entertainment/music.html">
+              <span class="material-symbols-outlined"> headphones </span>
+              Música
+            </a>
+            <a href="../entertainment/videos.html">
+              <span class="material-symbols-outlined"> movie </span>
+              Videos
+            </a>
+            <a href="../tools/pomodoro.html">
+              <span class="material-symbols-outlined"> timer </span>
+              Pomodoro
+            </a>
+            <a href="../entertainment/memes.php">
+              <span class="material-symbols-outlined">
+                sentiment_very_satisfied
+              </span>
+              Memes
+            </a>
+            <a href="../tools/all.html">
+              <span class="material-symbols-outlined"> description </span>
+              Ver todo
+            </a>
+          </div>
         </div>
       </div>
 
-      <div class="search position-relative text-center px-4 py-3 mt-2">
-        <input
-          type="text"
-          class="form-control w-100 border-0 bg-transparent"
-          placeholder="Buscar aqui"
-          style="color: #fff"
-        />
-        <i class="fa fa-search position-absolute d-block fs-6"></i>
-      </div>
+      <div class="main">
+        <div class="search-bar" style="height: 130px">
+          <input type="text" placeholder="Search" />
+        </div>
+        <div class="main-container">
+          <div class="timeline">
+            <!-- posts -->
+            <div class="timeline-right">
+              <form
+                class="status box"
+                method="POST"
+                action="../backend/add.post.php"
+                enctype="multipart/form-data"
+              >
+                <div class="status-menu">
+                  <div class="button-wrapper">
+                    <span class="label"> Subir imagen</span>
+                    <input
+                      type="file"
+                      name="imagen"
+                      id="upload"
+                      class="upload-box"
+                      placeholder="Upload File"
+                    />
+                  </div>
+                </div>
+                <!-- save data post -->
+                <div class="status-main">
+                  <input
+                    type="text"
+                    placeholder="Titulo..."
+                    class="title-post"
+                    name="title-post"
+                  />
+                  <textarea
+                    class="status-textarea"
+                    placeholder="Escribe algo..."
+                    name="data-post-text"
+                  >
+                  </textarea>
+                </div>
+                <!-- close -->
+                <div class="status-actions" id="abre">
+                  <input
+                    class="status-share"
+                    type="submit"
+                    name="submit"
+                    value="Compartir ahora"
+                  />
+                </div>
+              </form>
+              <!-- close form -->
 
-      <ul class="categories list-unstyled">
-        <li class=""><i class="bi bi-house-door"></i><a href="#">Inicio</a></li>
-        <br />
-        <li class="">
-          <i class="bi bi-mailbox"></i><a href="#">Buzón de sugerencias</a>
-        </li>
-        <br />
-        <li class="">
-          <i class="bi bi-music-note-beamed"></i><a href="#">Música</a>
-        </li>
-        <br />
-        <li class="">
-          <i class="bi bi-camera-video"></i><a href="#">Videos</a>
-        </li>
-        <br />
-        <li class="">
-          <i class="bi bi-chat-square-dots-fill"></i><a href="#">Contacto</a>
-        </li><br>  
-        <li class="">
-          <i class="bi bi-person-x-fill"></i><a href="#">Salir</a>
-        </li>
-      </ul>
-    </aside>
+              <style>
+                .title-post {
+                  border: 2px #fff;
+                  outline: none;
+                  padding: 4px;
+                  background: transparent;
+                  color: #fff;
+                }
+                .title-post::placeholder {
+                  color: #fff;
+                }
 
-    <!-- upload files here -->
-    <h1 class="title">Upload your images</h1>
-    <form action="../backend/addImage.php" method="post" class="form-group form" enctype="multipart/form-data">
-      <div class="input-group mb-3">
-        <input
-          type="file"
-          class="form-control"
-          id="inputGroupFile02"
-          name="imagen"
-        />
-        <input type="text" name="name-image" placeholder="Nombre de la imagen" autocomplete="off">
-        <button type="submit" class="btn btn-primary" name="btn-submit">Subir</button>
-      </div>
-    </form>
+                .button-wrapper {
+                  position: relative;
+                  width: 150px;
+                  text-align: center;
+                }
 
-    <!-- preview image user -->
-    <div class="preview-image">
-      <img class="imagePreview" src="" alt="">
-    </div>
+                .button-wrapper span.label {
+                  position: relative;
+                  z-index: 0;
+                  display: inline-block;
+                  width: 100%;
+                  background: #00bfff;
+                  cursor: pointer;
+                  color: #fff;
+                  padding: 10px 0;
+                  text-transform: uppercase;
+                  font-size: 12px;
+                }
 
-    <!-- images users-->
-  <h1 class="text-recent">Mas recientes</h1>
-           <div class="container" style="position:relative; left: 100px;">
+                #upload {
+                  display: inline-block;
+                  position: absolute;
+                  z-index: 1;
+                  width: 100%;
+                  height: 50px;
+                  top: 0;
+                  left: 0;
+                  opacity: 0;
+                  cursor: pointer;
+                }
+              </style>
+
+              <!-- render data -->
               <?php   
                   include("../backend/bd.php");    
-                  $query = "SELECT * FROM imagenes";
+                  $query = "SELECT * FROM posts";
                   $result = mysqli_query($conex, $query);
-
                   while ($fila = mysqli_fetch_array($result)) {
-              ?>
-              <div class="tech-box">
-                  <img src="<?php echo $fila['ruta'];?>" alt="" style="width: 300px; height:300px;">
+               ?>
+              <div class="card">
+                <div class="card-header">
+                  <img
+                    src="https://via.placeholder.com/50"
+                    alt="Profile Picture"
+                  />
+                  <div class="header-text">
+                    <h3><?php echo $fila['title_post']; ?></h3>
+                    <p><?php echo $fila['fecha']?></p>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <p>
+                    <?php echo $fila['description_post']; ?>
+                  </p>
+                  <!-- foto del post -->
+                  <img
+                    src="<?php echo $fila['ruta_file'];?>"
+                    alt="Post Image"
+                  />
+                  <!-- <button class="options-post-comment">Ayudar</button> -->
+                </div>
               </div>
-              <!-- close keys -->
-              <?php
-                  }
-              ?>
+              <?php } ?>
+
               <style>
-                .tech-box {
-                  overflow: hidden;
-                  margin: 10px 20px;
-                  border-radius: 20px;
+                .options-post-comment {
+                  cursor: pointer;
+                  padding: 10px;
+                  position: relative;
+                  border: none;
+                  border-radius: 4px;
+                }
+
+                .form__group {
+                  position: relative;
+                  padding: 15px 0 0;
+                  margin-top: 10px;
+                  width: 50%;
+                }
+
+                .form__field {
+                  font-family: inherit;
+                  width: 100%;
+                  border: 0;
+                  border-bottom: 2px solid #9b9b9b;
+                  outline: 0;
+                  font-size: 1.3rem;
+                  color: #fff;
+                  padding: 7px 0;
+                  background: transparent;
+                  transition: border-color 0.2s;
+                }
+                .form__field::placeholder {
+                  color: transparent;
+                }
+                .form__field:placeholder-shown ~ .form__label {
+                  font-size: 10px;
+                  cursor: text;
+                  top: 20px;
+                }
+
+                .form__label {
+                  position: absolute;
+                  top: 0;
+                  display: block;
+                  transition: 0.2s;
+                  color: #9b9b9b;
+                }
+
+                .form__field:focus {
+                  padding-bottom: 6px;
+                  font-weight: 700;
+                  border-width: 3px;
+                  border-image: linear-gradient(to right, #11998e, #38ef7d);
+                  border-image-slice: 1;
+                }
+                .form__field:focus ~ .form__label {
+                  position: absolute;
+                  top: 0;
+                  display: block;
+                  transition: 0.2s;
+                  font-size: 1rem;
+                  color: #11998e;
+                  font-weight: 700;
+                }
+
+                /* reset input */
+                .form__field:required,
+                .form__field:invalid {
+                  box-shadow: none;
+                }
+
+                .card {
+                  background-color: #151728;
+                  border-radius: 10px;
+                  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+                  max-width: 600px;
+                  margin: 20px auto;
+                }
+
+                @media (max-width: 600px) {
+                  .card {
+                    width: 100%;
+                    border-radius: 0;
+                  }
+                }
+
+                .card-header {
+                  display: flex;
+                  align-items: center;
+                  padding: 20px;
+                }
+
+                .card-header img {
+                  border-radius: 50%;
+                  margin-right: 20px;
+                }
+
+                .card-header h3 {
+                  font-size: 18px;
+                  font-weight: bold;
+                  margin: 0;
+                }
+
+                .card-header p {
+                  font-size: 14px;
+                  color: #aaa;
+                  margin: 0;
+                }
+
+                .card-body {
+                  padding: 20px;
+                }
+
+                .card-body p {
+                  margin-bottom: 20px;
+                }
+
+                .card-body img {
+                  max-width: 100%;
+                  border-radius: 10px;
+                }
+
+                .card-footer {
+                  display: flex;
+                  justify-content: space-between;
+                  align-items: center;
+                  padding: 20px;
+                }
+
+                .card-footer button {
+                  background-color: #fff;
+                  border: none;
+                  color: #444;
+                  font-size: 16px;
                   cursor: pointer;
                 }
 
-                .tech-box img{
-                  width: 100%;
-                  object-fit: cover;
+                .card-footer button:hover {
+                  color: #4267b2;
+                }
+
+                @media (max-width: 600px) {
+                  .card-footer {
+                    flex-direction: column;
+                    align-items: flex-start;
+                    padding: 10px 20px;
+                  }
+
+                  .card-footer button {
+                    margin-bottom: 10px;
+                  }
                 }
               </style>
-          </div>  
-    </main>
-    <!-- <materialize -- -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <!-- bootstrap -->
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-      crossorigin="anonymous"
-    ></script>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="overlay"
+        @click="rightSide = false; leftSide = false"
+        :class="{ 'active': rightSide || leftSide }"
+      ></div>
+    </div>
+     <script src="../scripts/menu_index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="../scripts/functions_index.js"></script>
   </body>
 </html>
